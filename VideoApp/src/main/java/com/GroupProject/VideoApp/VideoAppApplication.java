335feac6f34cf117d3b3bc125520e0463fdc8282
MNinh <msninh2@gmail.com>
2021-04-23 16:27:36 -0400

    project start

---
+++ VideoApp/src/main/java/com/GroupProject/VideoApp/VideoAppApplication.java
@@ -0,0 +1,13 @@
+package com.GroupProject.VideoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoAppApplication.class, args);
+	}
+
+}
---
+++ VideoApp/src/test/java/com/GroupProject/VideoApp/VideoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GroupProject.VideoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
