d6e0bfe1f337531c6ed2001667910327a65aaba3
Chris Allen <callen7908@yahoo.com>
2021-04-26 08:10:15 -0400

    Did work on VideoController

---
+++ VideoApp/src/main/java/com/GroupProject/VideoApp/controllers/VideoController.java
@@ -0,0 +1,44 @@
+package com.GroupProject.VideoApp.controllers;
+
+import com.GroupProject.VideoApp.models.Video;
+import com.GroupProject.VideoApp.services.VideoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class VideoController {
+
+    private final VideoService service;
+
+    public VideoController(VideoService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/video")
+    public ResponseEntity<Video> createVideo(@RequestBody Video video){
+        return new ResponseEntity<>(service.add(video), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/video/{id}")
+    public ResponseEntity<Video> updatePerson(@PathVariable Long id, Video video){
+        return new ResponseEntity<>(service.update(id,video),HttpStatus.OK);
+    }
+
+    @GetMapping("/video/{id}")
+    public ResponseEntity<Video> getPerson(@PathVariable Long id){
+        return new ResponseEntity<>(service.getOne(id),HttpStatus.OK);
+    }
+
+    @GetMapping("/video")
+    public ResponseEntity<Iterable<Video>> getAllPeople(){
+        return new ResponseEntity<>(service.getAll(),HttpStatus.OK);
+    }
+
+    @DeleteMapping("/video/{id}")
+    public ResponseEntity<Boolean> deletePerson(@PathVariable Long id){
+        return new ResponseEntity<>(service.remove(id),HttpStatus.OK);
+    }
+
+
+}
