86679e4749791b6a466a23e8f5c4f5d1a6866fe7
MNinh <msninh2@gmail.com>
2021-04-26 13:56:35 -0400

    updated User

--- VideoApp/src/main/java/com/GroupProject/VideoApp/models/User.java
+++ VideoApp/src/main/java/com/GroupProject/VideoApp/models/User.java
@@ -1,12 +1,79 @@
 package com.GroupProject.VideoApp.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class User {
-    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     Long userId;
+    String email;
+    String userName;
+    String password;
+    String firstName;
+    String lastName;
+
+
+
+    public User() {
+    }
+
+    public User(Long userId, String email, String userName, String password, String firstName, String lastName) {
+        this.userId = userId;
+        this.email = email;
+        this.userName = userName;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
 }
