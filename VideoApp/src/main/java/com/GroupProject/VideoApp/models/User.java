a9037e49dc4add7be731aa1170d6002b790018e9
Chris Allen <callen7908@yahoo.com>
2021-04-26 07:46:02 -0400

    Video entity created

--- VideoApp/src/main/java/com/GroupProject/VideoApp/models/User.java
+++ VideoApp/src/main/java/com/GroupProject/VideoApp/models/User.java
@@ -1,4 +1,12 @@
 package com.GroupProject.VideoApp.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    Long userId;
 }
