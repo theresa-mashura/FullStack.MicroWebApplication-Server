cc7ea44e9a95e3c8509df6984b976cc6a64afafd
Theresa <theresamashura@gmail.com.address>
2021-04-29 23:12:39 -0400

    Small settings change, video posting to S3 now working

--- VideoApp/src/main/java/com/GroupProject/VideoApp/controllers/StorageController.java
+++ VideoApp/src/main/java/com/GroupProject/VideoApp/controllers/StorageController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.annotation.MultipartConfig;
+
 @RestController
 @RequestMapping("/file")
 public class StorageController {
--- VideoApp/src/main/java/com/GroupProject/VideoApp/services/StorageService.java
+++ VideoApp/src/main/java/com/GroupProject/VideoApp/services/StorageService.java
@@ -8,9 +8,11 @@ import com.amazonaws.util.IOUtils;
 import lombok.extern.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.annotation.MultipartConfig;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -24,6 +26,7 @@ public class StorageService {
 
     @Autowired
     private AmazonS3 s3Client;
+
 
     public String uploadFile(MultipartFile file) {
         File fileObj = convertMultiPartFileToFile(file);
